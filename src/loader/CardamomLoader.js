ecb578779ff2f72eee1f48d601f26606a6cac4fc
RamEmmadi <ekkaemmadi@gmail.com>
2021-10-17 10:56:38 -0500

    2 files modified, 2 added

--- src/loader/CardamomLoader.js
+++ src/loader/CardamomLoader.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './_loader.scss';
 
-const CardamomLoader = () => {
+const CardamomLoader = (props) => {
+  const loaderSize = `loader-wrapper loader-wrapper__${props.size}`;
+
   return (
-    <div className="loader-wrapper loader-wrapper__big">
+    <div className={loaderSize}>
       <span className="hori-bar"></span>
     </div>
   );
+};
+
+CardamomLoader.propTypes = {
+  size: PropTypes.oneOf(['small', 'medium', 'big']),
+};
+
+CardamomLoader.defaultProps = {
+  size: 'small',
 };
 
 export default CardamomLoader;
