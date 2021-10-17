2bb87a98bfae6afcf21fbea91677f5cc0ebe908c
RamEmmadi <ekkaemmadi@gmail.com>
2021-10-17 09:45:22 -0500

    2 files modified, 1 added

--- src/loader/CardamomLoader.js
+++ src/loader/CardamomLoader.js
@@ -3,8 +3,8 @@ import './_loader.scss';
 
 const CardamomLoader = () => {
   return (
-    <div className="loader-wrapper">
-      <div className="hori-bar"></div>
+    <div className="loader-wrapper loader-wrapper__small">
+      <span className="hori-bar"></span>
     </div>
   );
 };
